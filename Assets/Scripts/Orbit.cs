d1d2c0f62d71ccdba9a578dde1edfa5b8db95791
matthew gimbl <mgimbl.dev@gmail.com>
2017-06-10 20:57:25 -0500

    Added the player object, player controller, the orbit spheres and script to make them orbit the parent

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public GameObject Parent;
+    public int Speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    transform.RotateAround(Parent.transform.position, Vector3.up, Speed * Time.deltaTime);
+    }
+}
